144225bb0e86258514afd48eaca27e6d28f6fb4d
Евгений Смиленко <smilenko.e@gmail.com>
2025-03-09 10:36:28 +0200

    Update BotLogic

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,11 +1,15 @@
-﻿using Telegram.Bot;
+﻿using System.Collections.Concurrent;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 using TelegramBot;
 
 //TODO: Add a normal logic? don`t use AI
 public static class Program
 {
+    private static readonly ConcurrentDictionary<long, string> UserMessages = new ConcurrentDictionary<long, string>();
+
     public static void Main()
     {
         var telegramBot = new TelegramBotClient(Helper.GetToken());
@@ -18,9 +22,31 @@ public static class Program
 
     public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        if (update.Type == UpdateType.Message)
+        if (update.Type == UpdateType.Message && update.Message.Type == MessageType.Text)
         {
-            var message = update.Message;
+            // Save the user's message
+            UserMessages[update.Message.Chat.Id] = update.Message.Text;
+
+            var inlineKeyboard = new InlineKeyboardMarkup(new[]
+            {
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Calculate", "CalculateButton"),
+                }
+            });
+
+            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Push the button", replyMarkup: inlineKeyboard, cancellationToken: cancellationToken);
+        }
+        else if (update.Type == UpdateType.CallbackQuery && update.CallbackQuery.Data == "CalculateButton")
+        {
+            // Retrieve the user's message
+            if (UserMessages.TryGetValue(update.CallbackQuery.Message.Chat.Id, out var userMessage))
+            {
+                //TODO: Add validation data 
+                //var str = userMessage;
+                //var pattern = @"()"
+
+            }
         }
     }
 
@@ -29,6 +55,4 @@ public static class Program
         // TODO: Add logging
         return Task.CompletedTask;
     }
-
-    
 }
